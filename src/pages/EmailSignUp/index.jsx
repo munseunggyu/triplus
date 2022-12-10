147b50b797623779b571de0eca6a42cfa1cf6bf4
DaeYeob Kim <kimdyk2@hanmail.net>
2022-12-10 16:17:26 +0900

    build: 초기환경세팅

---
+++ src/pages/EmailSignUp/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EmailSignUp() {
+  return <div>EmailSignUp</div>;
+}
---
+++ src/pages/MyProfileAddProduct/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyProfileAddProduct() {
+  return <div>MyProfileAddProduct</div>;
+}
