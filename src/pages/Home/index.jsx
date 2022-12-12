19708d344eb17fe238793ebc79e77d336fbe52a2
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-12-12 18:01:03 +0900

    feat: MainContainer padding-bottom 값 수정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import HeaderTitle from "../../components/Header/HeaderTitle";
+import Prev from "../../components/Header/Prev";
 import SearchButton from "../../components/Header/SearchButton";
 import Vertical from "../../components/Header/Vertical";
 import { MainContainer } from "../../components/MainContainer";
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Header>
-        <HeaderTitle>피드</HeaderTitle>
+        <HeaderTitle>메인 홈페이지입니다.</HeaderTitle>
         <SearchButton />
       </Header>
       <MainContainer></MainContainer>
