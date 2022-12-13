9dc660feb0fb6551f896abec9f80fd81487f4c2d
jeongyeon.seo <s9530348@naver.com>
2022-12-13 14:47:06 +0900

    feat: PostUpload 페이지 프리뷰 사진, 프리뷰 버튼 추가

--- src/pages/PostUpload/index.jsx
+++ src/pages/PostUpload/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import Prev from "../../components/Header/Prev";
@@ -6,37 +6,60 @@ import uploadBtn from "../../assets/images/upload_button.png";
 import { MainContainer } from "../../components/MainContainer";
 import smallProfile from "../../assets/images/user_img_small.png";
 import uploadFile from "../../assets/images/upload-file.png";
+import xIcon from "../../assets/images/x.png";
 
+const UploadContainer = styled.div`
+  display: flex;
+  padding-top: 16px;
+  gap: 13px;
+  min-height: 100px;
+`;
+
+const UploadContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+  width: 100%;
+`;
+
+const SmallProfileImg = styled.div`
+  background: url(${smallProfile}) no-repeat;
+  display: block;
+  width: 42px;
+  height: 42px;
+`;
+
+const UploadText = styled.textarea`
+  width: 100%;
+  height: 20vh;
+  border: none;
+  resize: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const UploadContentImg = styled.div`
+  background: ${(props) =>
+    props.uploadImg
+      ? `center / cover
+    url("https://cdn.pixabay.com/photo/2019/12/15/13/01/couple-4697055_1280.jpg")
+    no-repeat`
+      : ""};
+  display: block;
+  width: 300px;
+  height: 200px;
+  border-radius: 20px;
+  margin-top: 16px;
+`;
+
+// button
 const UploadBtn = styled.button`
   background: url(${uploadBtn}) no-repeat;
   background-size: 90px;
   margin-left: auto;
   width: 90px;
   height: 32px;
-`;
-
-const UploadContainer = styled.div`
-  padding-top: 20px;
-`;
-
-const UploadImg = styled.div`
-  background: ${smallProfile};
-  background-size: 42px;
-  display: inline-block;
-  width: 42px;
-  height: 42px;
-`;
-
-const UploadTextarea = styled.textarea`
-  position: fixed;
-  padding-left: 13px;
-  width: 100%;
-  height: 10vh;
-  border: none;
-  resize: none;
-  &:focus {
-    outline: none;
-  }
 `;
 
 const UploadFileBtn = styled.button`
@@ -50,7 +73,19 @@ const UploadFileBtn = styled.button`
   height: 50px;
 `;
 
+const DeleteBtn = styled.button`
+  position: relative;
+  bottom: 205px;
+  left: 270px;
+  background: ${(props) => (props.uploadImg ? `url(${xIcon})` : "")};
+  background-size: 22px;
+  width: 22px;
+  height: 22px;
+`;
+
 export default function PostUpload() {
+  let [uploadImg, setUploadImg] = useState(true);
+
   return (
     <div>
       <Header>
@@ -59,8 +94,21 @@ export default function PostUpload() {
       </Header>
       <MainContainer>
         <UploadContainer>
-          <UploadImg />
-          <UploadTextarea placeholder="게시글 입력하기..." />
+          <SmallProfileImg />
+          <UploadContentWrapper>
+            <UploadText placeholder="게시글 입력하기..." />
+
+            <UploadContentImg uploadImg={uploadImg} />
+
+            <DeleteBtn
+              uploadImg={uploadImg}
+              onClick={() => {
+                setUploadImg((prev) => {
+                  return !prev;
+                });
+              }}
+            />
+          </UploadContentWrapper>
           <UploadFileBtn />
         </UploadContainer>
       </MainContainer>
