cdbe41c5f212e9273f6ae8291c4a8ac4035c4799
밤밤 <jiiyo0@naver.com>
2022-12-13 11:46:15 +0900

    feat: ProductUpload 페이지 추가

---
+++ src/pages/ProductUpload/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../components/Header";
+import Prev from "../../components/Header/Prev";
+
+export default function ProductUpload() {
+	return (
+		<div>
+			<Header>
+				<Prev />
+			</Header>
+		</div>
+	);
+}
