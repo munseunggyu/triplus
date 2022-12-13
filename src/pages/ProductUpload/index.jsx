6879e6d89e6f5de5e0dad971f4e82cc9ff9119a8
밤밤 <jiiyo0@naver.com>
2022-12-13 13:23:46 +0900

    feat: productupload 버튼 이미지 추가

--- src/pages/ProductUpload/index.jsx
+++ src/pages/ProductUpload/index.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import Prev from "../../components/Header/Prev";
+import saveBtn from "../../assets/images/save_button.png";
+import imgFile from "../../assets/images/file_gray.png";
+
+const SaveBtn = styled.button`
+	background-image: ${saveBtn};
+	background-size: 90px;
+	margin-left: auto;
+	width: 90px;
+	height: 32px;
+`;
 
 export default function ProductUpload() {
 	return (
 		<div>
 			<Header>
 				<Prev />
+				<SaveBtn />
 			</Header>
 		</div>
 	);
