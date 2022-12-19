9a4ba232c5c95ece4a921f2a49458a925c06b66c
DaeYeob Kim <kimdyk2@hanmail.net>
2022-12-19 23:32:08 +0900

    style: route 재설정 & 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
 import { Route, Routes } from "react-router-dom";
-import Profile from "./pages/Profile";
-import SetProfile from "./pages/SetProfile";
+import SplashScreen from "./pages/SplashScreen";
 import MultiLogin from "./pages/MultiLogin";
+import EmailLogin from "./pages/EmailLogin";
+import EmailSignUp from "./pages/EmailSignUp";
+import MyProfileEdit from "./pages/MyProfileEdit";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Follow from "./pages/Follow";
+import SetProfile from "./pages/SetProfile";
 import PostDetail from "./pages/PostDetail";
 import PostUpload from "./pages/PostUpload";
-import Home from "./pages/Home";
-import SplashScreen from "./pages/SplashScreen";
 import ProductUpload from "./pages/ProductUpload";
-import Follow from "./pages/Follow";
-import ErrorPage from "./pages/ErrorPage";
-import Search from "./pages/Search";
-import ChatRoom from "./pages/ChatRoom";
 import ChatList from "./pages/ChatList";
+import ChatRoom from "./pages/ChatRoom";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-	return (
-		<div>
-			<Routes>
-				<Route path="/profile" element={<Profile />} />
-				<Route path="/editprofile" element={<SetProfile />} />
-				<Route path="/multiLogin" element={<MultiLogin />} />
-				<Route path="/postupload" element={<PostUpload />} />
-				<Route path="/postdetail" element={<PostDetail />} />
-				<Route path="/" element={<Home />} />
-				<Route path="/splash" element={<SplashScreen />} />
-				<Route path="/productupload" element={<ProductUpload />} />
-				<Route path="/profile/:accountname/following" element={<Follow />} />
-				<Route path="/profile/:accountname/follower" element={<Follow />} />
-				<Route path="/404" element={<ErrorPage />} />
-				<Route path="/search" element={<Search />} />
-				<Route path="/chatroom" element={<ChatRoom />} />
-				<Route path="/chatlist" element={<ChatList />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div>
+      <Routes>
+        <Route path="/splash" element={<SplashScreen />} />
+        <Route path="/multilogin" element={<MultiLogin />} />
+        <Route path="/emaillogin" element={<EmailLogin />} />
+        <Route path="/emailsignup" element={<EmailSignUp />} />
+        <Route path="/myprofileedit" element={<MyProfileEdit />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:accountname/following" element={<Follow />} />
+        <Route path="/profile/:accountname/follower" element={<Follow />} />
+        <Route path="/editprofile" element={<SetProfile />} />
+        <Route path="/postdetail" element={<PostDetail />} />
+        <Route path="/postupload" element={<PostUpload />} />
+        <Route path="/productupload" element={<ProductUpload />} />
+        <Route path="/chatlist" element={<ChatList />} />
+        <Route path="/chatroom" element={<ChatRoom />} />
+        <Route path="/404" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
