873df507b4200caa0f75442719333a7e546edd16
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-12-26 16:10:19 +0900

    refactor: 프로필 페이지 스타일 분리

--- src/pages/Profile/BottomSection.jsx
+++ src/pages/Profile/BottomSection.jsx
@@ -4,39 +4,15 @@ import styled from "styled-components";
 import PostCard from "../../components/PostCard";
 import { useParams } from "react-router-dom";
 import { useReloadData } from "../../hooks/useReloadData";
+import * as S from "./style";
 
-const ProfileBottomSectionBtns = styled.div`
-  width: 390px;
-  padding: 12px 0;
-  margin: 0 auto;
-  text-align: end;
-`;
-const PostAlbumtIcon = styled.button`
-  width: 26px;
-  height: 26px;
-  background: url(${css_sprite}) -10px -10px;
-`;
-const PostListIcon = styled.button`
-  width: 26px;
-  height: 26px;
-  background: url(${css_sprite}) -56px -56px;
-`;
-const Line = styled.div`
-  margin-bottom: 16px;
-  border-bottom: 2px solid #f2f2f2;
-`;
-const CardContainer = styled.ul`
-  padding: 0 21px;
-`;
 export default function ProfileBottomSection() {
   const { accountname } = useParams();
   const bottomRef = useRef(null);
-  const [isAlbum, setIsAlbum] = useState(false);
   const { skip, bottomBoolean, data, isLoding, bottomScroll, getData } =
     useReloadData(bottomRef, 800);
   const [trigger, setTrigger] = useState(false);
   const url = `${process.env.REACT_APP_API_KEY}/post/${accountname}/userpost/?limit=10&skip=${skip}`;
-  console.log(isAlbum);
   useEffect(() => {
     if (bottomBoolean) {
       getData(url, true);
@@ -51,16 +27,16 @@ export default function ProfileBottomSection() {
   return isLoding ? null : (
     <section>
       <h2 className="ir">사용자가 작성한 게시글</h2>
-      <ProfileBottomSectionBtns>
-        <PostListIcon onClick={() => setIsAlbum(false)} />
-        <PostAlbumtIcon onClick={() => setIsAlbum(true)} />
-      </ProfileBottomSectionBtns>
-      <Line />
-      <CardContainer ref={bottomRef}>
+      <S.ProfileBottomSectionBtns>
+        <S.PostListIcon />
+        <S.PostAlbumtIcon />
+      </S.ProfileBottomSectionBtns>
+      <S.Line />
+      <S.CardContainer ref={bottomRef}>
         {data.post.map((post) => {
           return <PostCard key={post.id} setTrigger={setTrigger} {...post} />;
         })}
-      </CardContainer>
+      </S.CardContainer>
     </section>
   );
 }
--- src/pages/Profile/TopSectionYour.jsx
+++ src/pages/Profile/TopSectionYour.jsx
@@ -1,33 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import css_sprite from "../../assets/images/css_sprites.png";
 import IsFollowButton from "../Follow/IsFollowButton";
-
-const YourProfileBtnCon = styled.div`
-  display: flex;
-  gap: 12px;
-  justify-content: center;
-`;
-
-const YourProfileBtn = styled(Link)`
-  width: 34px;
-  height: 34px;
-  box-sizing: border-box;
-  border: ${(props) => `1px solid ${props.theme.borderColor}`};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  span {
-    width: 20px;
-    height: 20px;
-    background: ${(props) =>
-      props.isfollowicon
-        ? `url(${css_sprite}) -130px -190px`
-        : `url(${css_sprite}) -90px -190px`};
-  }
-`;
+import * as S from "./style";
 
 export default function TopSectionYour({
   isfollow,
@@ -35,21 +10,21 @@ export default function TopSectionYour({
   setProfile,
 }) {
   return (
-    <YourProfileBtnCon>
-      <YourProfileBtn>
+    <S.YourProfileBtnCon>
+      <S.YourProfileBtn>
         <span className="ir">채팅하기</span>
         <span />
-      </YourProfileBtn>
+      </S.YourProfileBtn>
       <IsFollowButton
         isfollow={isfollow}
         userAccountName={userAccountName}
         isProfile="1"
         setProfile={setProfile}
       />
-      <YourProfileBtn isfollowicon="1">
+      <S.YourProfileBtn isfollowicon="1">
         <span className="ir">채팅하기</span>
         <span />
-      </YourProfileBtn>
-    </YourProfileBtnCon>
+      </S.YourProfileBtn>
+    </S.YourProfileBtnCon>
   );
 }
