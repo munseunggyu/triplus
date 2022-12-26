ae83e6475d086e16ce5b767882e92a0f5f4347a3
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-12-26 16:20:57 +0900

    refactor: 게시글 카드 스타일 분리

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -10,65 +10,8 @@ import { useNavigate } from "react-router-dom";
 import { useModal } from "../../hooks/useModal";
 import { handleDelete } from "../../utils/handleDelete";
 import { handleDeclaration } from "../../utils/handleDeclaration";
+import * as S from "./style";
 
-const PostCardList = styled.li`
-  list-style: none;
-  max-width: 358px;
-  width: 100%;
-  display: flex;
-  gap: 12px;
-  margin: 0 auto 24px;
-  position: relative;
-  &:first-child {
-    padding-top: 20px;
-  }
-`;
-
-const PostCardUserImg = styled.img`
-  width: 42px;
-  height: 42px;
-  border-radius: 50%;
-  object-fit: cover;
-  cursor: pointer;
-`;
-const PostCardUserName = styled.strong`
-  margin-top: 4px;
-  font-size: 16px;
-  display: block;
-  font-weight: 500;
-`;
-const PostCardUserId = styled.span`
-  font-size: 12px;
-  color: ${(props) => props.theme.grayColor};
-`;
-const PostCardContentTxt = styled.pre`
-  font-size: 14px;
-  margin-bottom: 20px;
-  line-height: 1.4;
-  white-space: -moz-pre-wrap;
-  white-space: -pre-wrap;
-  white-space: -o-pre-wrap;
-  white-space: pre-wrap;
-  word-wrap: break-word;
-  margin-top: 16px;
-`;
-const PostCardContentImg = styled.img`
-  width: 100%;
-  border-radius: 10px;
-`;
-
-const PostCardTime = styled.time`
-  font-size: 12px;
-  color: ${(props) => props.theme.grayColor};
-`;
-
-const PostCardVertical = styled.button`
-  position: absolute;
-  right: 0;
-  width: 18px;
-  height: 18px;
-  background: url(${css_sprites}) -170px -190px;
-`;
 export default function PostCard({
   id,
   author,
@@ -103,27 +46,27 @@ export default function PostCard({
 
   return (
     <>
-      <PostCardList>
-        <PostCardUserImg
+      <S.PostCardList>
+        <S.PostCardUserImg
           src={author.image.includes("Ellipse") ? user_img_small : author.image}
         />
         <div>
-          <PostCardUserName>{author.username}</PostCardUserName>
-          <PostCardUserId>&#64;{author.accountname}</PostCardUserId>
-          <PostCardContentTxt>{content}</PostCardContentTxt>
-          {image && <PostCardContentImg src={image} alt="게시물 이미지" />}
+          <S.PostCardUserName>{author.username}</S.PostCardUserName>
+          <S.PostCardUserId>&#64;{author.accountname}</S.PostCardUserId>
+          <S.PostCardContentTxt>{content}</S.PostCardContentTxt>
+          {image && <S.PostCardContentImg src={image} alt="게시물 이미지" />}
           <PostCardBtns
             postkey={id}
             commentCount={commentCount}
             heartCount={heartCount}
             hearted={hearted}
           />
-          <PostCardTime>{createAtFormat} </PostCardTime>
+          <S.PostCardTime>{createAtFormat} </S.PostCardTime>
         </div>
-        <PostCardVertical onClick={handleModal}>
+        <S.PostCardVertical onClick={handleModal}>
           <span className="ir">더보기 버튼</span>
-        </PostCardVertical>
-      </PostCardList>
+        </S.PostCardVertical>
+      </S.PostCardList>
       {isModal && (
         <ModalContainer onClick={handleModal}>
           {isMyContent ? (
