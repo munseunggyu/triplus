f8bc37779034a94d7a0512b179dc7c3d3426399e
jeongyeon.seo <s9530348@naver.com>
2022-12-27 02:56:25 +0900

    style: commentbar 스타일 파일 분리

--- src/components/CommentBar/index.jsx
+++ src/components/CommentBar/index.jsx
@@ -1,50 +1,7 @@
-import styled from "styled-components";
-import user_img_small from "../../assets/images/user_img_small.svg";
 import axios from "axios";
 import { useState } from "react";
-
-const CommentContainer = styled.div`
-  position: fixed;
-  bottom: 0;
-  background-color: #fff;
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 36px;
-  padding: 13px 0;
-  border-top: 0.5px solid ${(props) => props.theme.borderColor};
-`;
-
-const CommentForm = styled.form`
-  display: flex;
-  width: 100%;
-`;
-
-const CommentProfileImg = styled.img`
-  width: 36px;
-  padding: 0 18px 0 16px;
-`;
-
-const CommentInput = styled.input`
-  border: 0;
-  &:focus {
-    outline: none;
-  }
-  width: 100%;
-
-  &::placeholder {
-    font-size: 14px;
-    color: #c4c4c4;
-  }
-`;
-
-const CommentBtn = styled.button`
-  box-sizing: border-box;
-  font-size: 14px;
-  color: ${(props) => (props.txt ? props.theme.mainColor : "#c4c4c4")};
-  width: 5em;
-  text-align: center;
-`;
+import * as S from "./style";
+import user_img_small from "../../assets/images/user_img_small.svg";
 
 export default function CommentBar({ postkey, setCommentList }) {
   const userInfo = JSON.parse(localStorage.getItem("userinfo"));
@@ -81,17 +38,17 @@ export default function CommentBar({ postkey, setCommentList }) {
   };
 
   return (
-    <CommentContainer>
-      <CommentForm onSubmit={postComment}>
-        <CommentProfileImg src={user_img_small} alt="사용자 이름" />
-        <CommentInput
+    <S.CommentContainer>
+      <S.CommentForm onSubmit={postComment}>
+        <S.CommentProfileImg src={user_img_small} alt="사용자 이름" />
+        <S.CommentInput
           type="text"
           placeholder="댓글 입력하기..."
           onChange={handlePostComment}
           value={txt}
         />
-        <CommentBtn txt={txt}>게시</CommentBtn>
-      </CommentForm>
-    </CommentContainer>
+        <S.CommentBtn txt={txt}>게시</S.CommentBtn>
+      </S.CommentForm>
+    </S.CommentContainer>
   );
 }
