77c6272af341b913212a20eaf603f46429b34280
밤밤 <jiiyo0@naver.com>
2023-01-16 00:10:48 +0900

    style: locationInfo에 margin-top추가

--- src/components/LocationInfo/style.js
+++ src/components/LocationInfo/style.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
 export const LocationInfoContainer = styled.div`
-  display: flex;
-  border: 1px solid ${(props) => props.theme.mainColor};
-  border-radius: 10px;
-  padding: 17px 16px;
-  gap: 14px;
-  max-width: 304px;
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 6px;
-    strong {
-      font-size: 14px;
-    }
-    address {
-      font-size: 10px;
-    }
-  }
-  a {
-    align-self: flex-end;
-    margin-left: auto;
-    font-size: 12px;
-  }
+	display: flex;
+	border: 1px solid ${(props) => props.theme.mainColor};
+	border-radius: 10px;
+	padding: 17px 16px;
+	gap: 14px;
+	max-width: 304px;
+	margin-top: 10px;
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 6px;
+		strong {
+			font-size: 14px;
+		}
+		address {
+			font-size: 10px;
+		}
+	}
+	a {
+		align-self: flex-end;
+		margin-left: auto;
+		font-size: 12px;
+	}
 `;
